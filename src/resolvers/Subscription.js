76e9f7aaa51d6915c6d399a00ed1507ab4c5c2d7
akashgiricse <akashgiricse@gmail.com>
2019-06-12 14:51:33 +0530

    Add comment subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -8,7 +8,20 @@ const newPost = {
       return payload
     },
   }
+
+
+  function newCommentSubscribe(parent, args, context, info) {
+    return context.prisma.$subscribe.comment({ mutation_in: ['CREATED' ] }).node()
+  }
+
+  const newComment = {
+    subscribe: newCommentSubscribe,
+    resolve: payload => {
+      return payload
+    },
+  }
   
   module.exports = {
     newPost,
+    newComment,
   }
