5953fa155534a92127c69bc0dc3d09d18fbafb17
akashgiricse <akashgiricse@gmail.com>
2019-06-14 13:00:19 +0530

    Define POSTS_QUERY in a seperate const

--- frontend/graphql-blog/src/Posts.js
+++ frontend/graphql-blog/src/Posts.js
@@ -4,25 +4,22 @@ import gql from 'graphql-tag';
 
 import Post from './Post';
 
-const POSTS = gql`
-
-
+const POSTS_QUERY = gql`
+  {
+    post {
+      id
+      title
+      body
+    }
+  }
 `;
 
 const Posts = () => (
-    <Query query={gpl`{
-    
-            post {
-                id
-                title 
-                body
-            }
-    }
-    `}>
+    <Query query={POSTS_QUERY}>
     
             {({ loading, error, data}) => {
                 if(loading) return <p>Loading ...</p>
-                if(error) console.log(error);
+                if(error) return `Error! ${error.message}`;
                 console.log(data)
                 return data.post.map((currentPost) => (
                     <Post post={currentPost} />
